9f78d57f85f8527045fdc9eb14a15284ab24b3ce
Justin Leung <Justin@justinleung.net>
2011-03-12 23:47:16 -0500

    Completed the layout structure

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,14 @@
 class PagesController < ApplicationController
   def home
     @title = "Home"
-    @header = "Sample App"
   end
 
   def contact
     @title = "Contact"
-    @header = "Contact Me"
   end
   
   def about
     @title = "About"
-    @header = "About Us"
   end
 
 end
